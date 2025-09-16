bbdce05986b97b68e075b57d17436aa51a538c08
bortolo <andrea.bortolossi89@gmail.com>
2025-09-16 10:28:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-import json
-import plotly.express as px
-
 def lambda_handler(event, context):
-    # genera un grafico semplice
-    fig = px.line(x=[1, 2, 3], y=[4, 5, 6], title="Grafico da Lambda")
-    html_div = fig.to_html(full_html=True)
-
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/html"
-        },
-        "body": html_div
+        "headers": {"Content-Type": "text/plain"},
+        "body": "Hello from Lambda!"
     }
